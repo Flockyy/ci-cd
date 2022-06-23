d3643966a692cbe511d2a169e011ba617e5b4c6c
Florian ABGRALL <florian.abgralldwwb@gmail.com>
2022-06-23 16:22:06 +0200

    '

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__, template_folder='templates')
 
-usersList = ['Aaron', 'Bianca', 'Cat', 'Danny', 'Elena']
+client_id = os.getenv('GOOGLE_CLIENT_ID')
+client_secret = os.getenv('GOOGLE_CLIENT_SECRET')
+app.secret_key=os.getenv('secret_key')
 
-# @app.route('/')
-# def index():
-#     return render('Hello World')
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
-@app.route('/', methods=['GET', 'POST'])
+blueprint = make_google_blueprint(
+    client_id=client_id,
+    client_secret=client_secret,
+    reprompt_consent=True,
+    scope=['profile', 'email']
+)
+
+app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.route('/')
 def index():
-    return render_template('base.html')
+    google_data = None
+    user_info_endpoint='/oauth2/v2/userinfo'
+    if google.authorized:
+        google_data=google.get(user_info_endpoint).json()
 
-@app.route('/users', methods=['GET'])
-def users():
-    return jsonify({ 'users': [user for user in usersList] })
+    return render_template('base.html',
+        google_data = google_data,
+        fetch_url=google.base_url+user_info_endpoint
+    )
 
-@app.route('/user/<int:id>', methods=['GET'])
-def userById(id):
-    return jsonify({ 'username': usersList[id]  })
-
-@app.route('/user/<string:name>', methods=['GET'])
-def getUserByName(name):
-    # Show some user information
-    return "Some info"
-
-@app.route('/user/<string:name>', methods=['POST'])
-def addUserByName(name):
-    usersList.append(name)
-    return jsonify({ 'message': 'New user added'  })
+@app.route('/login')
+def login():
+    return redirect(url_for('google.login'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='myfakedomain.com', port=5000, debug=True)
